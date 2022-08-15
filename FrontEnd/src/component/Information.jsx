9399ac0b44d4533aa665785055c3295ab5befb44
Aroosa-A <aroosabanoahmed.gmail.com>
2022-08-15 11:21:08 +0100

    committing profile and information components

---
+++ FrontEnd/src/component/Information.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+
+
+const Information = () => {
+    return (
+        <>
+            <h1>Your Information</h1>
+            <h3>Badges:</h3>
+            <div className='badge1'>
+                <h2>Badge A</h2>
+                <p>xxxxx xxx x</p>
+            </div>
+            <div className='badge2'>
+                <h2>Badge A</h2>
+                <p>xxxxx xxx x</p>
+            </div>
+            <div className='badge3'>
+                <h2>Badge A</h2>
+                <p>xxxxx xxx x</p>
+            </div>
+            <div className='badge4'>
+                <h2>Badge A</h2>
+                <p>xxxxx xxx x</p>
+            </div>
+        </>
+    );
+}
+
+
+
+export default Information;
---
+++ FrontEnd/src/component/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+    // return (
+    // <> className= ="Basic-info"
+    //     <div>
+    //         <h1>Your profile</h1>
+    //         <button type="submit" id="edit-button">Edit</button>
+    //         <p>Name: Nathan</p>
+    //         <p>Personal email: nathanfry98@gmail.com</p>
+    //         <p>Digital futures email: nathan@dfutures.com</p>
+    //         <p>Github: nate8998</p>
+    //         <p>Linkdin: nathan@linkden</p>
+    //         <p>Phone: 07872415240</p>
+    //         ssName="Summary"cla</div>
+    //     <div>
+    //         <
+    //     <div className="info-box">h2>Personal story summary:</h2>
+    //         <br />
+    //         <p>Degree in Chemistry</p>
+    //         <p>13 school qualifications</p>
+    //         <p>5 work experience</p>
+    //     </div>
+    //     <p>3 personal achievements</p>
+    // </div>
+    // </>
+    // )
+}
+export default Profile
