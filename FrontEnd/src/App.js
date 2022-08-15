9b8136f63fdfd5ff429b8e8f0276cce90a48c480
Nathan Fry <nathanfry98@gmail.com>
2022-08-15 13:39:11 +0100

    profile-component

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 // import './App.css';
-
+import profilecss from './CSS/Profile.css'
 import Information from "./component/Information";
 import Profile from "./component/Profile";
 
