1b6cbb84e5ddf38e29b60afadf741cbd1fa9b5a3
gabiblack <gabiblack22@gmail.com>
2022-08-15 14:42:15 +0100

    tests for header component

---
+++ FrontEnd/src/tests/Header.test.js
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import Header from "../Components/Header";
+import React from "react";
+
+describe(`header tests`, () => {
+  
+    test(`That the DFX logo is rendered`, () => {
+        render(
+            <Header />
+        );
+
+        expect(screen.getByTestId("dfx-logo")).toBeInTheDocument();
+    });
+
+    test(`That the Need Help link is rendered`, () => {
+        render(
+            <Header />
+        );
+
+        expect(screen.getByTestId("need-help-link")).toBeInTheDocument();
+    });
+
+    test(`That the Menu link is rendered`, () => {
+        render(
+            <Header />
+        );
+
+        expect(screen.getByTestId("menu-link")).toBeInTheDocument();
+    });
+
+    test(`That the profile picture is rendered`, () => {
+        render(
+            <Header />
+        );
+
+        expect(screen.getByTestId("profile-pic-nav")).toBeInTheDocument();
+    });
+
+});
