e9ac0fc52081b196ed370161033b6940fc12db18
Dafne Ribeiro <106087250+DafneRibeiro@users.noreply.github.com>
2022-08-16 10:17:18 +0100

    css for header

--- FrontEnd/src/Components/Header.jsx
+++ FrontEnd/src/Components/Header.jsx
@@ -1,15 +1,18 @@
 import logo from "../imgs/IMG_1184.png";
+import '../CSS/Header.css';
 
 const Header = () => {
   return (
 
     <header>
       <nav>
-        <div className="header">
-          <img src={logo} alt="DFX Logo" width="50" height="50" />
-          <a href="#">Need help? Talk to us!</a>
-          <a href="#">Menu</a>
-          <img src="" alt="" />
+        <div className="header-box">
+          <img className="Logo" src={logo} alt="DFX Logo" width="50" height="50" />
+          <div className="top-links">
+          <a className="help" href="#">Need help? Talk to us!</a>
+          <a className="menu" href="#">Menu</a>
+            <img className="profile-headerImg" src="" alt="DR" />
+            </div>
         </div>
       </nav>
     </header>
