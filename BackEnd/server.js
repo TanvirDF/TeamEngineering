fa81c2a01209dbe7f1eabd1d4d3a7bf1131a92f9
TanvirDF <ahamedtanvir619@gmail.com>
2022-08-17 12:30:07 +0100

    Db is connected to the server

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import mangoose from 'mongoose'
+import mongoose from 'mongoose'
 import { router as graduate } from './routes/graduate.js'
 import { router as profile } from './routes/profile.js'
 
@@ -12,9 +12,13 @@ const db = process.env.DB_URI;
 const app = express();
 
 
+const main = async () => {
+    console.log(`Connecting to DB @ ${db}`);
+    await mongoose.connect(db);
+}
+main().catch(err => console.log(err))
 
-
-
+//##### TEST by requesting http://127.0.0.1:4000/graduate/1 
 app.use('/graduate', graduate)
 app.use('/profile', profile)
 
