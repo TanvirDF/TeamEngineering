bea1c361b99bff65fab0ccd0b20849be5a6fed0c
Dafne Ribeiro <106087250+DafneRibeiro@users.noreply.github.com>
2022-08-19 14:54:46 +0100

    data server

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Footer from '../src/Components/Footer.jsx';
-import Header from '../src/Components/Header.jsx'
+import Footer from '../src/Components/Footer';
+import Header from '../src/Components/Header'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './Components/HomePage/HomePage.jsx';
-import ProfilePage from './Components/ProfilePage/ProfilePage.jsx';
+import Homepage from '../src/Components/HomePage/HomePage';
+import ProfilePage from '../src/Components/ProfilePage/ProfilePage';
 
 // import { useEffect, useState } from 'react';
 // import axios from 'axios';
