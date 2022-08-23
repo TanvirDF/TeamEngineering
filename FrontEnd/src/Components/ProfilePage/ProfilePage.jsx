05a882c1dcb8db909fc6037a50eea1b0b6f9c64f
Dafne Ribeiro <106087250+DafneRibeiro@users.noreply.github.com>
2022-08-23 13:52:27 +0100

    tables2

--- FrontEnd/src/Components/ProfilePage/ProfilePage.jsx
+++ FrontEnd/src/Components/ProfilePage/ProfilePage.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import EditPI from "./EditPI";
-import PersonalStory from "./PersonalStory";
+import PersonalStory from "./PersonalStory.jsx";
 import ProfileCompletion from "./ProfileCompletion";
 // import "FrontEnd/src/CSS/EditPI.css";
-
     
 const ProfilePage = () => {
     return (
         <div>
         <ProfileCompletion/>
-        <PersonalStory />
         <EditPI/>
+        <PersonalStory />
         </div>
     )
 }
