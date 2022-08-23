c067ebbad23af08615e288d391cb4b90d71677f1
SeanWagstaffe <wagstaffesean@gmail.com>
2022-08-23 13:41:10 +0100

    userId added

--- BackEnd/models/training.model.js
+++ BackEnd/models/training.model.js
@@ -1,22 +1,26 @@
 const mongoose = require("mongoose");
 
 const trainingSchema = new mongoose.Schema({
-    cohort: { 
-        type: String, 
-        required: true 
+    userId: {
+        type: String,
+        required: true,
     },
-    track: { 
-        type: String, 
-        required: true 
+    cohort: {
+        type: String,
+        required: true
     },
-    trainer: { 
-        type: String, 
-        required: true 
+    track: {
+        type: String,
+        required: true
     },
-    finishDate: { 
-        type: Date, 
-        required: true 
-    } 
+    trainer: {
+        type: String,
+        required: true
+    },
+    finishDate: {
+        type: Date,
+        required: true
+    }
 })
 
 const Training = mongoose.model(`Training`, trainingSchema)
