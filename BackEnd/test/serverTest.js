367d978bf3ffedc1a3a5ca0f39ecbd90f2c5221e
TanvirDF <ahamedtanvir619@gmail.com>
2022-08-23 11:37:38 +0100

    some changes

--- BackEnd/test/serverTest.js
+++ BackEnd/test/serverTest.js
@@ -3,37 +3,36 @@ const { expect } = require('chai')
 const chaiHttp = require('chai-http')
 const server = require('../server.js')
 const Graduate = require("../models_take2/graduate.model");
-
-const testData = require('../models_take2/testUsers.json')
+const PersonalStory = require('../models_take2/personalStory.model.js');
+const graduateData = require('../models_take2/testUsers.json');
+const personalStoryData = require('../models_take2/personalStory.json')
 
 chai.use(chaiHttp)
 
 describe('testing the requests on database', () => {
-
-    beforeEach(async () => {
-        try {
-            await Graduate.deleteMany()
-            console.log('collection cleared')
-
-        } catch (err) {
-            console.log('error clearing the collection')
-            throw new Error()
-        }
-
-        try {
-            await Graduate.insertMany(testData)
-            console.log('data inserted')
-
-        } catch (err) {
-            console.log(`error populating the data ${err}`)
-            throw new Error()
-
-        }
-    })
-
     describe('Gradate', () => {
-        it('should GET a graduate given the id', async () => {
-            const id = testData[0].userId
+        beforeEach(async () => {
+            try {
+                await Graduate.deleteMany()
+                console.log('collection cleared')
+
+            } catch (err) {
+                console.log('error clearing the collection')
+                throw new Error()
+            }
+
+            try {
+                await Graduate.insertMany(graduateData)
+                console.log('data inserted')
+
+            } catch (err) {
+                console.log(`error populating the data ${err}`)
+                throw new Error()
+
+            }
+        })
+        it('should GET a graduate info given the id', async () => {
+            const id = graduateData[0].userId
             const res = await chai.request(server).get(`/graduate/${id}`).send()
             expect(res).to.have.status(200);
             expect(res.body).to.be.an(`object`);
@@ -42,7 +41,35 @@ describe('testing the requests on database', () => {
     })
 
     describe('Personal Story', () => {
+        beforeEach(async () => {
+            try {
+                await PersonalStory.deleteMany()
+                console.log('collection cleared')
+
+            } catch (err) {
+                console.log('error clearing the collection')
+                throw new Error()
+            }
+
+            try {
+                await PersonalStory.insertMany(personalStoryData)
+                console.log('data inserted')
+
+            } catch (err) {
+                console.log(`error populating the data ${err}`)
+                throw new Error()
+
+            }
+        })
+        it('should GET a personal story info given the id', async () => {
+            const id = graduateData[0].userId
+            const res = await chai.request(server).get(`/personalStory/${1234}`).send()
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an(`object`);
+        })
 
     })
 
+
 })
+
