128614235acc6d67420e980cddc43ca03b2c6c8a
gabiblack <gabiblack22@gmail.com>
2022-08-23 13:47:38 +0100

    updated training schema, data and info schema

--- BackEnd/models/training.model.js
+++ BackEnd/models/training.model.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
 
+const moduleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PASSED", "FAILED", "PENDING"],
+        required: false
+    }
+})
+
 const trainingSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
     cohort: { 
         type: String, 
         required: true 
@@ -16,7 +32,12 @@ const trainingSchema = new mongoose.Schema({
     finishDate: { 
         type: Date, 
         required: true 
-    } 
+    },
+    modules: {
+        type: [moduleSchema],
+        default: [],
+        required: false,
+    }
 })
 
 const Training = mongoose.model(`Training`, trainingSchema)
